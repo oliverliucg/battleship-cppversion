883623d88355053278613b493cc4dee0ecd3b238
Vincent <jimmysip@163.com>
2021-09-23 22:10:24 +0800

    abbrev.hpp

---
+++ Abbrev.hpp
@@ -0,0 +1,11 @@
+#include<cstdio>
+
+const String dottedLine = "=====================================";
+
+const String CoordinateForPlacement = "where do you want to place a ";
+const String collidesAnother = "Sorry, your coordinates collides with another ship, please input your coordinate again:";
+const String offGrid = "Sorry, your ship is going off the grid, please input your coordinate again:";
+
+const String actionAvail = "what would you like to do?/nF Fire at a square/nM Move a ship to another square (2 remaining)\nS Sonar scan (1 remaining)";
+const String coorToFire = "where do you want to fire?";
+const String hitCarrier = "You hit a carrier!";
