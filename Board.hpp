d017557e1bd3021211bea0c464ed3e609cda3868
Vincent <jimmysip@163.com>
2021-09-24 16:47:17 +0800

    ff

---
+++ Board.hpp
@@ -0,0 +1,17 @@
+#include "Ships.hpp"
+#include <vector>
+#include <set>
+#include <pair>
+#include "Point.hpp"
+class Board{
+ private:
+  int width;
+  int height;
+  std::vector<Ship>;
+  std::set<pair<char,char>>;
+ public:
+  Board():width(3),height(3){};
+  void fireShip(Point targetPoint);
+  void moveShip(Point targetPoint);
+  void getShip(Point targetPoint);
+};
---
+++ Point.hpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+#include <cstdlib>
+class Point{
+private:
+  int coor_x;
+  int coor_y;
+public:
+  Point(int x, int y):coor_x(x),coor_y(y){}
+  Point(){};
+};
